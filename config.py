58b6402f24af627dc10f9c4fda9781c340284443
Alemnew Marie <alemnewmarie461@gmail.com>
2022-07-16 10:51:18 +0300

    Migrate products resource

--- config.py
+++ config.py
@@ -5,17 +5,18 @@ load_dotenv()  # take environment variables from .env.
 
 
 class APP:
-    MAX_THREADS=int(os.getenv("MAX_THREADS",10))
+    MAX_THREADS = int(os.getenv("MAX_THREADS", 10))
+
 
 class WC:
-    STORE_URL=os.getenv("SITE")
-    CONSUMER_KEY=os.getenv("consumer_key")
-    CONSUMER_SECRET=os.getenv("consumer_secret")
+    STORE_URL = os.getenv("SITE")
+    CONSUMER_KEY = os.getenv("consumer_key")
+    CONSUMER_SECRET = os.getenv("consumer_secret")
+
 
 class DB:
-    MONGO_URI=os.getenv("MONGO_URI")
-    NAME=os.getenv("MONGO_DB")
-    ORDER_COLLECTION=os.getenv("ORDER_COLLECTION","orders")
-    CUSTOMER_COLLECTION=os.getenv("CUSTOMER_COLLECTION","vendors")
-
-
+    MONGO_URI = os.getenv("MONGO_URI")
+    NAME = os.getenv("MONGO_DB")
+    ORDER_COLLECTION = os.getenv("ORDER_COLLECTION", "orders")
+    CUSTOMER_COLLECTION = os.getenv("CUSTOMER_COLLECTION", "vendors")
+    PRODUCT_COLLECTION = os.getenv("PRODUCT_COLLECTION", "products")
--- migration.py
+++ migration.py
@@ -3,7 +3,7 @@ Command-line interface for the script
 """
 import click
 import datetime
-import customers, orders
+import customers, orders, products
 
 
 @click.group()
@@ -189,7 +189,95 @@ def import_customers(id, sort, after, before, days, hours, sync):
             customers.import_all_customers(sort, after, before, sync=False)
 
 
+@click.command("products")
+@click.option(
+    "--id",
+    "-i",
+    type=click.INT,
+    help="ID of specific product to be imported.",
+)
+@click.option(
+    "--sort",
+    "-s",
+    help="Product sort attribute ascending (asc) or descending (desc).",
+    default="desc",
+)
+@click.option("--after", "-a", help="ISO datetime to import products after (FROM)")
+@click.option("--before", "-b", help="ISO datetime to import products before (TO)")
+@click.option(
+    "--days",
+    "-d",
+    type=click.INT,
+    help="Import products created in the past X days (default=0 today)",
+    default=0,
+)
+@click.option(
+    "--hours",
+    "-h",
+    type=click.INT,
+    help="Import products created in the past X hours (default=1 hour)",
+    default=1,
+)
+@click.option(
+    "--sync",
+    is_flag=True,
+    help="Sync records (insert ones that are not in the Database)",
+    default=False,
+)
+def import_products(id, sort, after, before, days, hours, sync):
+    """
+    Import all products created between a datetime range or specific product
+    """
+    if id:
+        print(f"Importing specific product with ID {id}")
+        products.get_product(id)
+        return
+
+    if sort:
+        if sort.startswith("asc"):
+            sort = "asc"
+        else:
+            sort = "desc"
+
+    if after and before:
+        print(
+            f"Importing all products created after '{after}' and before '{before}' sorted '{sort}'...\n"
+        )
+        products.import_all_products(sort, after, before)
+    else:
+        current_time = datetime.datetime.now()
+        today = datetime.date.today()
+        if days > 0:
+            # user has provided days argument
+            start_day = today - datetime.timedelta(days=days)
+        else:
+            # default is today
+            start_day = today
+
+        if start_day != today:
+            after = f'{str(start_day)}T{current_time.strftime("%H:%M:%S")}.000'
+        else:
+            if hours > 1:
+                # hours argument provided
+                start_time = current_time - datetime.timedelta(seconds=hours * 3600)
+            else:
+                # last 1 hour
+                start_time = current_time - datetime.timedelta(seconds=3600)
+
+            after = f'{start_time.strftime("%Y-%m-%dT%H:%M:%S")}.000'
+
+        before = f'{current_time.strftime("%Y-%m-%dT%H:%M:%S")}.000'
+        print(
+            f"Importing all products created after '{after}' and before '{before}' sorted '{sort}'...\n"
+        )
+        if sync:
+            products.import_all_products(sort, after, before, sync=True)
+        else:
+            products.import_all_products(sort, after, before, sync=False)
+
+
 cli.add_command(import_orders)
+cli.add_command(import_products)
 cli.add_command(import_customers)
 
 
